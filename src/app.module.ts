286be38c7019bdf764d03e512201fcfc3813370f
Roberto Carvajal <robertocarvajalvelasco@gmail.com>
2022-11-23 21:03:42 +0100

    Añadidos paquetes que faltaban

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MonumentoModule } from './monumento/monumento.module';
 
 @Module({
-  imports: [],
+  imports: [MonumentoModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/monumento/entities/monumento.entity.ts
@@ -0,0 +1,10 @@
+export class Monumento {
+  id: number;
+  nombreMonumento: string;
+  nombreCiudad: string;
+  nombrePais: string;
+  codigoPais: string;
+  localizacion: string;
+  descripcion: string;
+  linkFoto: string;
+}
